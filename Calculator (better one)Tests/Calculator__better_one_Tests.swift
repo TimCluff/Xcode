4532eeed0dac47553ec83ac8c76f7c752a41125c
Brady Cluff <bradycluff@Bradys-iMac.local>
2015-02-16 09:44:41 -0700

    Initial Commit

---
+++ Calculator (better one)Tests/Calculator__better_one_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Calculator__better_one_Tests.swift
+//  Calculator (better one)Tests
+//
+//  Created by Brady Cluff on 2/16/15.
+//  Copyright (c) 2015 tim. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Calculator__better_one_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
