7442483816af9ecab5adaa1b1b0e12c9743896a4
Allyssa and Brady Cluff <bradycluff@Bradys-MacBook-Pro.local>
2015-09-11 15:41:33 -0600

    Initial Commit

--- Calculator (better one)/ViewController.swift
+++ Calculator (better one)/ViewController.swift
@@ -14,7 +14,107 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet var dataBox: UILabel!
+    var firstNumber = (String(Int()) as NSString).floatValue
+    var secondNumber = (String(Int()) as NSString).floatValue
+    var operation = ""
+    var result = (String(Int()) as NSString).floatValue
+    var isTypingNumber = false
+    
+    func swag(var newValue:Float)
+    {
+        var getEnd = Range(start: (advance(toString(newValue).endIndex, -1)), end: toString(newValue).endIndex)
+        var isItAZero = toString(newValue).substringWithRange(getEnd)
+        if isItAZero.toInt() == 0
+        {
+            let number = Int(newValue)
+            dataBox.text = toString(number)
+        }
+        else
+        {
+            dataBox.text = toString(newValue)
+        }
+    }
+    
+    
+    @IBAction func clearButton(sender: AnyObject)
+    {
+        firstNumber = 0
+        secondNumber = 0
+        isTypingNumber = false
+        result = (String(0) as NSString).floatValue
+        dataBox.text = "\(0)"
+    }
+    @IBAction func positveNegative(sender: AnyObject) //Changes the Number to a negative number
+    {
+        var positiveNegative = dataBox.text?.toInt()
+        if dataBox.text > "\(0)"
+        {
+            dataBox.text = "\(positiveNegative! * -1)"
+        }
+        else
+        {
+            dataBox.text = "\(positiveNegative! * -1)"
+        }
+        
+    }
+    @IBAction func percentButton(sender: AnyObject) /*divdes the number by 100
+                                                     to take a percent*/
+    {
+        var string = String(dataBox.text!)
+        var data = (string as NSString).floatValue
+        dataBox.text = "\(data / 100)"
+    }
+    @IBAction func number(sender: AnyObject)
+    {
+        
+        var number = sender.currentTitle
+        if isTypingNumber == true
+        {
+            dataBox.text = dataBox.text! + number!!
+        }
+        else
+        {
+            dataBox.text = number
+        }
+        isTypingNumber = true
+    }
+    @IBAction func operation(sender: AnyObject)
+    {
+        isTypingNumber = false
+        firstNumber = Float(dataBox.text!.toInt()!)
+        operation = sender.currentTitle!!
+    }
+       @IBAction func decimal(sender: AnyObject)
+    {
+     
+    }
+    @IBAction func equalsButton(sender: AnyObject)
+    {
+        secondNumber = Float(dataBox.text!.toInt()!)
+        if operation == "+"
+        {
+            result = firstNumber + secondNumber
+            
+        }
+        else if operation == "-"
+        {
+            result = firstNumber - secondNumber
+            
+        }
+        else if operation == "X"
+        {
+            result = firstNumber * secondNumber
+         
+        }
+        else
+        {
+            result = firstNumber / secondNumber
+        
+        }
+        dataBox.text = "\(result)"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ formulas.swift
@@ -0,0 +1,9 @@
+//
+//  formulas.swift
+//  Calculator (better one)
+//
+//  Created by Brady Cluff on 5/28/15.
+//  Copyright (c) 2015 tim. All rights reserved.
+//
+
+import Foundation
